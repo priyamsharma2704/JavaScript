a2a9fd4f0f73e8f58d02d0664fb64c5e87b15255
priyamsharma2704 <priyamsharma2704@gmail.com>
2024-06-12 09:29:33 -0400

    updated Flatten-nd-array

---
+++ Flatten-n-D-Array/flatten-nd-array.js
@@ -0,0 +1,28 @@
+function flatten(arr)
+{
+    let result = [];
+    let stack = [...arr];
+
+    while(stack.length)
+    {
+        let item = stack.pop();
+
+        if(Array.isArray(item))
+        {
+            stack.push(...item);
+        }
+        else
+        {
+            result.push(item);
+        }
+    }
+
+    result = result.reverse();
+    return result;
+}
+
+console.log(flatten([[1, 2, 3], [2, 4, 5], [1, 2, 3, 4]]));
+
+//spreading operator.
+// arr = [1,2,3,4,5,6]
+// console.log(...arr)
