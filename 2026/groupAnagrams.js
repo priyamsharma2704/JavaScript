b05155e057e32372698f0fe661c2f1700be6d7a6
priyamsharma2704 <priyamsharma2704@gmail.com>
2026-03-08 00:14:51 -0500

    updated groupAnagrams

--- 2026/groupAnagrams.js
+++ 2026/groupAnagrams.js
@@ -11,4 +11,35 @@ Output: [["x"]]
 
 Input: strs = [""]
 Output: [[""]]
+
+space complexity : o(n*k) as for the map, n is the num of strings and k is the length of each string
+freq array and key array will have fixed space complexity o(1) as the size is fixed (26).
+
+time complexity: outer loop run for n times
+                 inner lopp runs for k times
+                 .join() take o(k) 
 */
+
+console.log(groupAnagram(["act", "pots", "tops", "cat", "stop", "hat"]));
+
+function groupAnagram(strs)
+{
+    var groups = new Map();
+
+    for (var str of strs)
+    {
+        var freq = new Array(26).fill(0);
+        for (var char of str)
+        {
+            freq[char.charCodeAt(0) - 97]++;
+        }
+
+        var key = freq.join(',');
+
+        if (!groups.get(key))
+            groups.set(key, []);
+        groups.get(key).push(str);
+    }
+
+    return Array.from(groups.values())
+}
