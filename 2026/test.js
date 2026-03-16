4a093430a7e090ac5a9bc0ddbdb99ccca1fbb408
priyamsharma2704 <priyamsharma2704@gmail.com>
2026-03-16 18:26:08 -0400

    test updated

--- 2026/test.js
+++ 2026/test.js
@@ -1,10 +1,17 @@
-isCapReached(userId)
+console.log(topKFrequent([1, 2, 2, 3, 3, 3], 2))
+function topKFrequent(arr, k)
 {
-    var value = redis.get(`user:${userId}:notif`)
-    if (value > CAP)
-        return true;
+    var res = [];
+    var freqMap = new Map();
+    for (var num of arr)
+        freqMap.set(num, (freqMap.get(num) || 0) + 1);
 
-    redis.incr('user:' + userId + ':notif')
-    redis.expire('user:' + userId + ':notif', 86400)
-    return false;
+    var bucket = new Array(arr.length + 1).fill().map(() => [])
+
+    for (var [key, value] of freqMap)
+        bucket[value].push(key);
+
+    for (var i = bucket.length - 1; i >= 0 && res.length < k; i--)
+        res.push(...bucket[i]);
+    return res
 }
