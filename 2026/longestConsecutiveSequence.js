5a8f794abe67c7adad87706b9178639a39ad2389
priyamsharma2704 <priyamsharma2704@gmail.com>
2026-03-19 18:36:31 -0400

    files updated

--- 2026/longestConsecutiveSequence.js
+++ 2026/longestConsecutiveSequence.js
@@ -1,18 +1,19 @@
 console.log(lcs([2, 20, 4, 10, 3, 4, 5]));
 console.log(lcs([0, 3, 2, 5, 4, 6, 1, 1]));
 console.log(lcs([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6])); // 7
+console.log(lcs([0, 0, -1])); //2
 
 
 function lcs(arr)
 {
-    var length = 0;
     var numSet = new Set(arr);
     var longest = 0;
     for (var num of arr)
     {
+        var length = 0;
         if (!numSet.has(num - 1))
         {
-            length = 1;
+            length++;
             while (numSet.has(num + length))
                 length++;
         }
